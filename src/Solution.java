ef7fd24b05674606de1cb571ad5aa5886d18f624
mehedi-01 <mehedihasan376766@gmail.com>
2023-03-25 15:23:56 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            for (int x = 0; x<n; x++){
+                a =  a+b;
+                System.out.println(a+" ");
+                b = b*2;
+            }
+            System.out.println("");
+        }
+        in.close();
+    }
+}
